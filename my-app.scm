c2125d35a866cece840018b600ee08a5a84daadf
Justin Ethier <justin.ethier@gmail.com>
2020-11-06 20:01:38 -0500

    WIP, added TODO

--- my-app.scm
+++ my-app.scm
@@ -154,11 +154,16 @@
       (log-error (string-append "Error calling route-to-controller for " url ":") err)
       (send-error-response "An error occurred"))
     (let* ((path-parts (url->path-parts url))
-           (ctrl-part (car path-parts))
+           (ctrl-part (if (pair? path-parts)
+                          (car path-parts)
+                          "index")) ;; Default controller
            (id-parts (if (> (length path-parts) 2)
                          (cddr path-parts)
-                         '()))
+                         '("index"))) ;; Default action
           )
+
+TODO: need to modify/generate path-parts with index parts inserted
+
       (log-notice 
         (list `(controller ,ctrl-part) 
               `(action ,(path-parts->action path-parts))
@@ -191,19 +196,22 @@
 ;;       FCGI service (default behavior)
 
 ;; TESTING
-#;(begin
+(begin
 ;  ;; No controller, do we provide a default one?
 ;  (route-to-controller "http://10.0.0.4/" "GET") (newline)
 ;  ;; No action, should have a means of default
 ;  (route-to-controller "http://10.0.0.4/demo" "GET") (newline)
 ;  (route-to-controller "http://10.0.0.4/demo/" "GET") (newline)
 ;  ;; ID arguments, should provide them. also should error if mismatched (too many/few for controller's action
-  (route-to-controller "http://10.0.0.4/demo/test/1/2/3" "GET") (newline)
-  (route-to-controller "http://10.0.0.4/demo/test2/arg1/arg2" "GET") (newline)
-  (route-to-controller "http://10.0.0.4/demo/status" "GET") (newline)
-  (route-to-controller "http://10.0.0.4/demo2/test" "GET") (newline)
-  (route-to-controller "http://10.0.0.4/controller/action/id" "GET") (newline)
-  (route-to-controller "http://localhost/demo.cgi" "GET") (newline)
+;  (route-to-controller "http://10.0.0.4/demo/test/1/2/3" "GET") (newline)
+;  (route-to-controller "http://10.0.0.4/demo/test2/arg1/arg2" "GET") (newline)
+;  (route-to-controller "http://10.0.0.4/demo/status" "GET") (newline)
+;  (route-to-controller "http://10.0.0.4/demo2/test" "GET") (newline)
+;  (route-to-controller "http://10.0.0.4/controller/action/id" "GET") (newline)
+;  (route-to-controller "http://localhost/demo.cgi" "GET") (newline)
+
+;; TODO: get this to work with top-level index, then another with ctrl index
+  (route-to-controller "http://localhost/" "GET") (newline)
 
   (exit 0)
 ) ;; END
