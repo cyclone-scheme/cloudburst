a3245b8c52c7210134e2d6ef20179141553e7515
Justin Ethier <justin.ethier@gmail.com>
2020-11-16 18:32:53 -0500

    Stub out the "cloudburst" program

---
+++ cloudburst.scm
@@ -0,0 +1,15 @@
+(import (scheme base) 
+        (scheme write)
+        )
+
+(define (usage)
+  (display
+    "Usage: cloudburst COMMAND
+
+  Commands:
+  
+    help - Display this usage text
+    ")
+)
+
+(usage)
---
+++ package.scm
@@ -0,0 +1,15 @@
+(package
+ (name           cloudburst)
+ (version        0.1)
+ (license        "MIT")
+ (authors        "Justin Ethier")
+ (maintainers    "Justin Ethier <justin.ethier at gmail dot com>")
+ (description    "The Cloudburst web framework")
+ (tags           "web" "networking")
+ (docs           "https://github.com/cyclone-scheme/cloudburst/wiki/sha2")
+ ;(test           "test-sha2.scm")
+
+ (program
+   (name cloudburst)
+   (description "Front-end program to make it easier to use Cloudburst"))
+)
