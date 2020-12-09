a7409eb85ca0949d497c62cee64be911e083b203
Justin Ethier <justin.ethier@gmail.com>
2020-12-09 14:57:37 -0500

    Initial file
    This may end up being moved to another type of script (.sh or .scm)

---
+++ scripts/create-db.sql
@@ -0,0 +1,9 @@
+CREATE TABLE task
+(
+  id serial,
+  body text NOT NULL, -- Textual description of the task
+  priority integer NOT NULL DEFAULT 0, -- Potentially used to sort tasks
+  done boolean NOT NULL DEFAULT false, -- Is the task done?
+  done_at timestamptz,
+  CONSTRAINT pk_task PRIMARY KEY (id)
+);
