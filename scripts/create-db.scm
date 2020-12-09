c86e6b308320fa73faf135150abac8a67cc405dc
Justin Ethier <justin.ethier@gmail.com>
2020-12-09 17:25:52 -0500

    Initial file

---
+++ scripts/create-db.scm
@@ -0,0 +1,10 @@
+(import (scheme base)
+        (scheme write)
+        (lib config)
+        (cyclone postgresql))
+
+;; TODO: load config/database.scm
+
+;; TODO: create table(s)
+
+;; TODO: load sample data
